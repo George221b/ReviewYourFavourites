5264c89f2ed254ef4a6c5e7a7b3e6092f52c7d05
George221b <george221b@gmail.com>
2018-01-05 19:50:04 +0200

    ReviewComic IndexView made

---
+++ ReviewYourFavourites.Web/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace ReviewYourFavourites.Web.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string SubstringWithThreeDots(this string text, int length)
+        {
+            if (text.Length > length)
+            {
+                return text.Substring(0, length) + "...";
+            }
+            else
+            {
+                return text;
+            }
+        }
+    }
+}
